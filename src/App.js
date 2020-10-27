e951b9398758db94d691dfe3052ae639a6ae0ea8
Matt Volonnino <mvolonnino12@gmail.com>
2020-10-27 16:06:28 -0400

    added spotify web api js, and getMe function to pull user data when login with Spotify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login/Login";
 import { getTokenFromUrl } from "./helpers/spotify/spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    const token = getTokenFromUrl();
-    console.log("TOKEN 🔑", token);
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("USER 👩‍💻", user);
+      });
+    }
+
+    console.log("TOKEN 🔑", { _token });
   }, []);
 
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
