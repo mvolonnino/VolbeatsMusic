27e41b0471bfa360ce1664721e7518acfbd4e77d
Matt Volonnino <mvolonnino12@gmail.com>
2020-10-27 15:39:47 -0400

    added img transform and a div that says a spotify clone when img is clicked

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Login.css";
 import VMLogo from "../../img/VolbeatsMusicLogo.png";
 import { loginUrl } from "../../helpers/spotify/spotify";
 
 function Login() {
+  const [showClone, setShowClone] = useState(false);
+  const showCloneDiv = () => {
+    setShowClone(!showClone);
+  };
   return (
     <div className="login">
-      <img src={VMLogo} alt="spotify logo" />
+      <img src={VMLogo} alt="spotify logo" onClick={showCloneDiv} />
+      {showClone ? <p>A Spotify Clone :)</p> : null}
       {/* Login with Spotify button */}
       <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
